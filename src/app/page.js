48195f6f6fe37cdecd6d4582c06d84ef1746b2e4
56snty <ek56351@gmail.com>
2025-11-23 19:51:10 +0100

    Fix: Update package.json deps and safe imports

--- src/app/page.js
+++ src/app/page.js
@@ -13,11 +13,15 @@ const manrope = Manrope({ subsets: ['latin'] })
 function StarField() {
   const ref = useRef()
   const [sphere] = useState(() => random.inSphere(new Float32Array(24000), { radius: 1.5 }))
+  
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10
-    ref.current.rotation.y -= delta / 15
-    ref.current.scale.z = 1 + Math.sin(state.clock.elapsedTime * 0.5) * 0.1
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10
+      ref.current.rotation.y -= delta / 15
+      ref.current.scale.z = 1 + Math.sin(state.clock.elapsedTime * 0.5) * 0.1
+    }
   })
+
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
       <Points ref={ref} positions={sphere} stride={3} frustumCulled={false}>
